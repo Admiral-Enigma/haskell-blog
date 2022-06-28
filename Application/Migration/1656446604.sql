df32fa963a1bc7342b9016078cd694cbdbce784a
Admiral-Enigma <lord-squirrel@protonmail.com>
2022-06-28 22:05:40 +0200

    Posts model

--- Application/Migration/1656446604.sql
+++ Application/Migration/1656446604.sql
@@ -3,14 +3,13 @@ BEGIN
     NEW.updated_at = NOW();
     RETURN NEW;
 END;
-$$ language plpgsql;
--- Your database schema. Use the Schema Designer at http://localhost:8001/ to add some tables.
+$$ language PLPGSQL;
 CREATE TABLE posts (
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
     title TEXT NOT NULL,
     body TEXT NOT NULL,
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
-    updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL
 );
 CREATE INDEX posts_created_at_index ON posts (created_at);
 CREATE TRIGGER update_posts_updated_at BEFORE UPDATE ON posts FOR EACH ROW EXECUTE FUNCTION set_updated_at_to_now();
