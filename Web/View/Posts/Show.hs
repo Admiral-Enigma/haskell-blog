46e331e8c1f81779b4639734e592caf45ebe970a
Admiral-Enigma <lord-squirrel@protonmail.com>
2022-06-28 22:58:32 +0200

    Markdown stuff

--- Web/View/Posts/Show.hs
+++ Web/View/Posts/Show.hs
@@ -1,6 +1,7 @@
 module Web.View.Posts.Show where
 import Web.View.Prelude
 import Web.View.Prelude (Post'(body, createdAt))
+import qualified Text.MMark as MMark
 
 data ShowView = ShowView { post :: Post }
 
@@ -9,7 +10,7 @@ instance View ShowView where
         {breadcrumb}
         <h1>{get #title post}</h1>
         <p>{get #createdAt post |> timeAgo}</p>
-        <div>{get #body post}</div>
+        <div>{get #body post |> renderMarkDown}</div>
 
     |]
         where
@@ -17,3 +18,7 @@ instance View ShowView where
                             [ breadcrumbLink "Posts" PostsAction
                             , breadcrumbText "Show Post"
                             ]
+renderMarkDown text = 
+    case text |> MMark.parse "" of
+        Left error -> "Something went wrong"
+        Right markdown -> MMark.render markdown |> tshow |> preEscapedToHtml
