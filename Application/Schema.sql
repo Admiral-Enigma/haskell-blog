c5528a4924264e19204a1619e7fbf48aa9f0c7ba
Admiral-Enigma <lord-squirrel@protonmail.com>
2022-06-28 23:06:49 +0200

    Comments table

--- Application/Schema.sql
+++ Application/Schema.sql
@@ -14,3 +14,11 @@ CREATE TABLE posts (
 );
 CREATE INDEX posts_created_at_index ON posts (created_at);
 CREATE TRIGGER update_posts_updated_at BEFORE UPDATE ON posts FOR EACH ROW EXECUTE FUNCTION set_updated_at_to_now();
+CREATE TABLE comments (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
+    post_id UUID NOT NULL,
+    author TEXT NOT NULL,
+    body TEXT NOT NULL
+);
+CREATE INDEX comments_post_id_index ON comments (post_id);
+ALTER TABLE comments ADD CONSTRAINT comments_ref_post_id FOREIGN KEY (post_id) REFERENCES posts (id) ON DELETE CASCADE;
